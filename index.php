292bb5ebc7105c8e0eae3b4bfc7c0de68d3f92ce
nb-mk2 <nicobaudis.net@gmail.com>
2024-11-14 11:01:30 -0300

    migro a PHP

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>SISTEMA SEA. UNLPAM </title>
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon.png">
-    <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.theme.default.min.css">
-    <link href="./vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
-    <link href="./css/style.css" rel="stylesheet">
-
-
-
-</head>
+<?php
+require_once "./head.php"; 
+?>
 
 <body>
 
@@ -42,19 +30,9 @@
         <!--**********************************
             Nav header start
         ***********************************-->
-        <div class="nav-header">
-            <a href="index.html" class="brand-logo">
-                <img class="logo-abbr" src="./images/logo.png" alt="">
-                
-                <img class="brand-title" src="./images/logo-letra.png" alt="">
-            </a>
-
-            <div class="nav-control">
-                <div class="hamburger">
-                    <span class="line"></span><span class="line"></span><span class="line"></span>
-                </div>
-            </div>
-        </div>
+        <?php
+        require_once "./nav_header.php"; 
+        ?>
         <!--**********************************
             Nav header end
         ***********************************-->
